fe266be2758cf7752b86f791415d759409ebedd4
Jonathan <jonathanguven@gmail.com>
2024-09-05 22:28:20 -0700

    implement jwt authentication

---
+++ backend/internal/utils/jwt.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var jwtSecret = []byte("temp-secret-key")
+
+// generate JWT token for user given their user ID
+func GenerateJWT(userID uint) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24 * 7).Unix(),
+	})
+	return token.SignedString(jwtSecret)
+}
+
+// set JWT token in HTTP-only cookie
+func SetCookie(w http.ResponseWriter, token string) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Value:    token,
+		HttpOnly: true,
+		Expires:  time.Now().Add(time.Hour * 24 * 7),
+		Path:     "/",
+		SameSite: http.SameSiteLaxMode,
+	})
+}
+
+// retrieve JWT token from a HTTP-only cookie
+func GetJWTFromCookie(r *http.Request) (*jwt.Token, error) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		return nil, err
+	}
+
+	return jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+}
