288eaa1017074fe9a670a7bd3de1830bfeb810c4
Jonathan <jonathanguven@gmail.com>
2024-09-06 14:34:57 -0700

    bug fix: cron job only queries guest urls

--- backend/internal/database/cleanup.go
+++ backend/internal/database/cleanup.go
@@ -10,7 +10,7 @@ import (
 
 // background job to periodically remove expired URLs
 func StartCleanup(db *gorm.DB) {
-	ticker := time.NewTicker(24 * time.Hour)
+	ticker := time.NewTicker(time.Minute)
 	go func() {
 		for range ticker.C {
 			cleanupExpiredURLs(db)
@@ -20,7 +20,7 @@ func StartCleanup(db *gorm.DB) {
 
 // remove URLs older than expiration date
 func cleanupExpiredURLs(db *gorm.DB) {
-	expired := db.Where("user_id AND expires_at < ?", time.Now()).Delete(&models.URL{})
+	expired := db.Where("user_id = 0 AND expires_at < ?", time.Now()).Delete(&models.URL{})
 	if expired.Error != nil {
 		log.Printf("Error cleaning up expired URLs: %v", expired.Error)
 		return
