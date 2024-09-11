52dddcde42cd6d3c635123ce0aafc25e420c9d09
Jonathan Nguyen <jonathanguven@gmail.com>
2024-09-10 19:47:35 -0700

    fix param extraction error

--- src/app/urls/[user]/page.tsx
+++ src/app/urls/[user]/page.tsx
@@ -3,7 +3,9 @@
 import { useParams } from 'next/navigation';
 
 const UserProfile = () => {
-  const { user } = useParams(); 
+  const params = useParams(); 
+  const user = params?.user as string | undefined;
+
   return (
     <div>
       <h1>Profile of {user}</h1>
