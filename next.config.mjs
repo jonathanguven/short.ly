9fb8ccd6581f8c0ae62c62ba72b5aa2ff1dbda46
Jonathan <jonathanguven@gmail.com>
2024-09-10 23:14:14 -0700

    ignore shortened url route on frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/s/:slug*',
+        destination: 'https://api.shrink.lol/s/:slug*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
