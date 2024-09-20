20cd6603088f4ccace26c958e227c4130ee1ce8b
Jonathan <jonathanguven@gmail.com>
2024-09-20 04:21:03 -0700

    fix wrong env var name

--- src/app/urls/columns.tsx
+++ src/app/urls/columns.tsx
@@ -68,7 +68,7 @@ export const columns: ColumnDef<URL>[] = [
       return (
         <div className="ml-4">
           <Link 
-            href={`${process.env.DOMAIN}/s/${alias}`} 
+            href={`${process.env.NEXT_PUBLIC_DOMAIN}/s/${alias}`} 
             target="_blank" 
             className="font-medium hover:underline"
           >
